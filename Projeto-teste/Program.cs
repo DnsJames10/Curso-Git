e988a157aa4330f4e8034f07ddac3512d23e02e3
Denis James <denisjamesmarangoni@hotmail.com>
2023-02-01 09:31:06 -0300

    Projeto iniciado, mostrando a mensagem ola mundo

---
+++ Projeto-teste/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d = new DateTime(2023, 01, 31, 15, 10, 11, DateTimeKind.Local);
+            DateTime d1 = new DateTime(2023, 01, 31, 15, 10, 11, DateTimeKind.Utc);
+
+            Console.WriteLine(d.ToLongTimeString());
+            Console.WriteLine(d.ToUniversalTime().ToShortTimeString());
+        }
+       
+    }
+}
